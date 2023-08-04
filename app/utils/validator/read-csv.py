834112a82ded376e07924c652cb791d0953cc604
ben-norton <ben.norton@naturalsciences.org>
2023-08-04 13:02:30 -0400

    Project cleanup, began yaml integration

---
+++ app/utils/validator/read-csv.py
@@ -0,0 +1,17 @@
+import csv
+import pandas as pd
+
+# open the file in read mode
+csvfile = '../../data/ltc/ltc-docs/ltc-terms-list.csv'
+
+data = pd.read_csv(csvfile)
+df = pd.DataFrame(data)
+uniques = df['datatype'].unique()
+print(uniques)
+
+# creating dictreader object
+file = open(csvfile, 'r')
+csv_reader = csv.DictReader(file)
+dict_from_csv = dict(list(csv_reader)[0])
+col_names = list(dict_from_csv.keys())
+print(col_names)
